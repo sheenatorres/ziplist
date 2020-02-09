31b38ec8c16718235202e2f093ddac61bcbba571
sheenatorres <59987784+sheenatorres@users.noreply.github.com>
2020-02-08 15:33:17 -1000

    Update ziplist.js

--- ziplist.js
+++ ziplist.js
@@ -8,6 +8,6 @@ function zipList(list1, list2) {
 console.log(zipList(['a', 'b', 'c'], [1, 2, 3]));
 
 function zipListTheSimpleWay(list1, list2) {
-  return _.zip(list1, list2);
+  return _.flatten(_.zip(list1, list2));
 }
 console.log(zipListTheSimpleWay(['a', 'b', 'c'], [1, 2, 3]));
